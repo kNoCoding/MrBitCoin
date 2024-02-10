3bc8ef27cc65812f8404fac63fa7ec74dc7b779b
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-10 19:54:06 +0200

    Add userService.js

---
+++ MrBitCoin/src/services/userService.js
@@ -0,0 +1,17 @@
+export const userService = {
+    getUser,
+}
+
+
+
+
+const user = {
+    name: "Puki Ben David",
+    balance: 100,
+    transactions: []
+}
+
+
+function getUser() {
+    return user
+}
