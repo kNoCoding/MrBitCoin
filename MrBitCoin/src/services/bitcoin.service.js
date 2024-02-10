5b7670454a466375b58e9f2c3cca9cff49b58cb6
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-10 21:02:21 +0200

    Implement getRate and test it

--- MrBitCoin/src/services/bitcoin.service.js
+++ MrBitCoin/src/services/bitcoin.service.js
@@ -1,11 +1,25 @@
 import axios from 'axios'
+import { storageService } from '../services/storage.service'
+
+const RATE_KEY = 'btcRate'
+
+const btcRateUrl = 'https://blockchain.info/tobtc?currency=USD&value=1&cors=true'
 
 export const bitcoinService = {
     getRate,
-    getMarketPriceHistory,
-    getAvgBlockSize,
+    // getMarketPriceHistory,
+    // getAvgBlockSize, 
 }
 
-function getRate() {
-
+async function getRate() {
+    const cachedRate = storageService.load(RATE_KEY)
+    if (cachedRate) return cachedRate
+    try {
+        const response = await axios.get(btcRateUrl)
+        storageService.save(RATE_KEY, response.data)
+        return response.data
+    } catch (error) {
+        console.error('Error fetching the exchange rate(btcRate):', error)
+        throw error
+    }
 }
--- MrBitCoin/src/views/AboutView.vue
+++ MrBitCoin/src/views/AboutView.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>btcRate: ${{ btcRate }}</p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script>
+import { bitcoinService } from '../services/bitcoin.service'
+
+export default {
+  // name: 'About',
+  data() {
+    return {
+      btcRate: null,
+    }
+  },
+  async mounted() {
+    try {
+      this.btcRate = await bitcoinService.getRate()
+    } catch (error) {
+      console.error('Error fetching the BTC rate:', error)
+    }
+  },
 }
-</style>
+</script>
+
+<style></style>
