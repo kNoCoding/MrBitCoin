433546f74441c8d8f891fca9044f04b869581287
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-11 18:08:04 +0200

    Implement bitcoin.service.js and test on AboutView.vue

--- MrBitCoin/src/services/bitcoin.service.js
+++ MrBitCoin/src/services/bitcoin.service.js
@@ -2,13 +2,17 @@ import axios from 'axios'
 import { storageService } from '../services/storage.service'
 
 const RATE_KEY = 'btcRate'
+const PRICE_HISTORY_KEY = 'marketPriceHistory'
+const BLOCK_SIZE_KEY = 'averageBlockSize'
 
-const btcRateUrl = 'https://blockchain.info/tobtc?currency=USD&value=1&cors=true'
+const btcRateUrl = 'https://blockchain.info/tobtc?currency=USD&value=1'
+const marketPriceHistoryUrl = 'https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true'
+const averageBlockSizeUrl = 'https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true'
 
 export const bitcoinService = {
     getRate,
-    // getMarketPriceHistory,
-    // getAvgBlockSize, 
+    getMarketPriceHistory,
+    getAvgBlockSize,
 }
 
 async function getRate() {
@@ -20,6 +24,36 @@ async function getRate() {
         return response.data
     } catch (error) {
         console.error('Error fetching the exchange rate(btcRate):', error)
+        throw error
+    }
+}
+
+async function getMarketPriceHistory() {
+    const cachedMarketPriceHistory = storageService.load(PRICE_HISTORY_KEY)
+    if (cachedMarketPriceHistory) return cachedMarketPriceHistory
+    try {
+        const response = await axios.get(marketPriceHistoryUrl)
+        storageService.save(PRICE_HISTORY_KEY, response.data)
+        console.log('response.data', response.data)
+
+        return response.data
+    } catch (error) {
+        console.error('Error fetching the market price history(averageBlockSize):', error)
+        throw error
+    }
+}
+
+async function getAvgBlockSize() {
+    const cachedAverageBlockSize = storageService.load(BLOCK_SIZE_KEY)
+    if (cachedAverageBlockSize) return cachedAverageBlockSize
+    try {
+        const response = await axios.get(averageBlockSizeUrl)
+        storageService.save(BLOCK_SIZE_KEY, response.data)
+        console.log('response.data', response.data)
+
+        return response.data
+    } catch (error) {
+        console.error('Error fetching the average block size(marketPriceHistory):', error)
         throw error
     }
 }
--- MrBitCoin/src/views/AboutView.vue
+++ MrBitCoin/src/views/AboutView.vue
@@ -2,6 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>btcRate: ${{ btcRate }}</p>
+    <p>marketPriceHistory: {{ marketPriceHistory }}</p>
+    <p>avgBlockSize: {{ avgBlockSize }}</p>
   </div>
 </template>
 
@@ -9,17 +11,37 @@
 import { bitcoinService } from '../services/bitcoin.service'
 
 export default {
-  // name: 'About',
+  name: 'About',
   data() {
     return {
       btcRate: null,
+      marketPriceHistory: null,
+      avgBlockSize: null,
     }
   },
   async mounted() {
     try {
       this.btcRate = await bitcoinService.getRate()
+      this.marketPriceHistory = await bitcoinService.getMarketPriceHistory()
+      console.log('this.marketPriceHistory', this.marketPriceHistory)
     } catch (error) {
       console.error('Error fetching the BTC rate:', error)
+    }
+
+    try {
+      this.marketPriceHistory = await bitcoinService.getMarketPriceHistory()
+      console.log('this.marketPriceHistory', this.marketPriceHistory)
+
+    } catch (error) {
+      console.error('Error fetching the market price history:', error)
+    }
+
+    try {
+      this.avgBlockSize = await bitcoinService.getAvgBlockSize()
+      console.log('this.avgBlockSize', this.avgBlockSize)
+
+    } catch (error) {
+      console.error('Error fetching the average block size:', error)
     }
   },
 }
