0c3593bba0d3f58e57b6f3b5b9d4673b520a94e3
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-11 19:46:48 +0200

    Create and import app Header and Footer

---
+++ MrBitCoin/src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <small>im the footer</small>
+</template>
+
+<script></script>
+
+<style scoped></style>
---
+++ MrBitCoin/src/components/AppHeader.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <p class="logo">MrBitCoin</p>
+
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+            </ul>
+        </nav>
+
+        <p>logged in user name</p>
+
+        <p>bitcoin exchange rate</p>
+    </header>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: auto 1fr auto auto;
+    column-gap: 1rem;
+    align-items: center;
+
+    background-color: gray;
+    color: darkorange;
+
+    padding: .5rem 1rem;
+}
+
+.logo {
+    font-size: 1.5rem;
+    cursor: pointer;
+    border: 3px solid lightgray;
+    border-radius: .75rem;
+    padding: 0 .5rem;
+
+    &:hover {
+        background-color: darkorange;
+        color: lightgray;
+    }
+}
+
+ul {
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+    list-style: none;
+}
+
+li {
+    cursor: pointer;
+    font-weight: 500;
+}
+</style>
