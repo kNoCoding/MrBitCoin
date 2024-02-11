c4b200d3d1cf5fe46ea1e8757aa85ed1f8e03edc
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-11 19:53:04 +0200

    Add styling

--- MrBitCoin/src/components/AppFooter.vue
+++ MrBitCoin/src/components/AppFooter.vue
@@ -1,7 +1,19 @@
 <template>
-    <small>im the footer</small>
+    <footer>
+        <small>im the footer ㄟ( ▔, ▔ )ㄏ</small>
+    </footer>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+footer {
+    padding: .5rem 1rem;
+    margin-block-start: 1rem;
+
+    background-color: gray;
+    font-weight: 500;
+
+    text-align: center;
+}
+</style>
