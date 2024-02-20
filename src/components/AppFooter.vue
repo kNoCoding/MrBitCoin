97bc87b8f32f978579f09eb8c2413fc6f9b57d4c
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-20 21:40:22 +0200

    Test commit after deleting project folder accidentally

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <small>im the footer ㄟ( ▔, ▔ )ㄏ woo wee - working deploy? please? porfavor?? come on..</small>
+        <small>im the footer ㄟ( ▔, ▔ )ㄏ woo wee</small>
     </footer>
 </template>
 
