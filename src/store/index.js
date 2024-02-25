f5339cf9ed667df35495bef80f60898ff1d02f03
Konstantin Nochevny <kosnobiz@gmail.com>
2024-02-25 21:58:56 +0200

    Define loadContacts and removeContact

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { contactService } from '@/services/contact.service'
 import { createStore } from 'vuex'
 
 const options = {
@@ -5,20 +6,38 @@ const options = {
     state() {
         return {
             count: 0,
+            contacts: [],
         }
     },
     mutations: {
         increment(state, { by }) {
             state.count += by
-        }
+        },
+        setContacts(state, { contacts }) {
+            state.contacts = contacts
+        },
+        removeContact(state, { contactId }) {
+            const idx = state.contacts.findIndex(contact => contact._id === contactId)
+            state.contacts.splice(idx, 1)
+        },
     },
     actions: {
         async incrementLater({ commit }, { by }) {
             setTimeout(() => commit({ type: 'increment', by }), 1500)
-        }
+        },
+        async loadContacts({ commit }) {
+            const contacts = await contactService.getContacts()
+            commit({ type: 'setContacts', contacts })
+        },
+        async removeContact({ commit }, { contactId }) {
+            await contactService.deleteContact(contactId)
+            commit({ type: 'removeContact', contactId })
+
+        },
     },
     getters: {
-        count(state) { return state.count }
+        count(state) { return state.count },
+        contacts(state) { return state.contacts }
     }
 }
 const store = createStore(options)
